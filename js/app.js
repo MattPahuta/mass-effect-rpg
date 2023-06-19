489a28db798f23b260f30fbb64acf9a3e72a1b80
Matt Pahuta <mhpahuta@gmail.com>
2023-06-19 15:51:29 -0700

    Add character render function with demo data

--- js/app.js
+++ js/app.js
@@ -3,10 +3,43 @@
 
 
 // character data
-const hero {
+const hero = {
+  elementId: 'hero',
+  name: 'Wizard',
+  avatar: './assets/wizard.png',
+  health: 60,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
+}
+
+const monster = {
+  elementId: 'monster',
+  name: 'Orc',
+  avatar: './assets/orc.png',
+  health: 10,
+  diceRoll: [2],
+  diceCount: 1,
+}
+
+function renderCharacter(data) {
+  // destructure data object, assign variables
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  // build dice html with map and join
+  let diceHtml = diceRoll.map(dice => {
+    return `<div class="dice">${dice}</div>`
+  }).join('')
+
+
+  document.getElementById(elementId).innerHTML = `
+    <div class="character-card">
+      <h4 class="name">${name}</h4>
+      <img class="avatar" src="${avatar}">
+      <div class="health">health: ${health}</div>
+      <div class="dice-container">${diceHtml}</div>
+    </div>
+  `
 
 }
 
-const monster {
-  
-}
+renderCharacter(hero);
+renderCharacter(monster);
